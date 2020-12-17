91da0a88ed30f537563724abcf377449e860f106
Miguel Gallego <Miguel.0.1.1.2.3.5.8@gmail.com>
2020-12-17 22:00:07 +0100

    Doing more

---
+++ MPlusStore/Posts/PostVC.swift
@@ -0,0 +1,30 @@
+//
+//  PostVC.swift
+//  MPlusStore
+//
+//  Created by Miguel on 17/12/2020.
+//  Copyright © 2020 Miguel Gallego Martín. All rights reserved.
+//
+
+import UIKit
+
+class PostVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MPlusStore/Products/Product.swift
@@ -0,0 +1,10 @@
+//
+//  Product.swift
+//  MPlusStore
+//
+//  Created by Miguel on 17/12/2020.
+//  Copyright © 2020 Miguel Gallego Martín. All rights reserved.
+//
+
+import Foundation
+
