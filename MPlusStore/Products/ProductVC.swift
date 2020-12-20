f051b02677cdc8d80085ca59b186060d4f6629f1
Miguel Gallego <Miguel.0.1.1.2.3.5.8@gmail.com>
2020-12-20 01:54:32 +0100

    Buy alert done

--- MPlusStore/Products/ProductVC.swift
+++ MPlusStore/Products/ProductVC.swift
@@ -14,12 +14,14 @@ class ProductVC: UIViewController {
     @IBOutlet weak var lblName: UILabel!
     @IBOutlet weak var lblDescr: UILabel!
     @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var btnBuyNow: UIButton!
     
     var product: Product!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         if product == nil {
+            btnBuyNow.isEnabled = false
             return
         }
         navBar.topItem?.title = product.name
@@ -34,6 +36,14 @@ class ProductVC: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func onBtnBuyNow() {
+        let msg = "\(product.name) ha sido añadido a tu cesta"
+        let alertVC = UIAlertController(title: "Cesta", message: msg, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertVC.addAction(okAction)
+        present(alertVC, animated: true, completion: nil)
+        
+    }
     
     
  
